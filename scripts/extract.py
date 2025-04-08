c02723d2f69986699fa4142f4d8b7b2a09e3d940
Enock <108288904+Enockjoao@users.noreply.github.com>
2025-04-08 16:01:36 -0300

    Update extract.py

--- scripts/extract.py
+++ scripts/extract.py
@@ -2,7 +2,7 @@ import pandas as pd
 import requests
 
 def extract_data(data):
-    # Carregar o arquivo CSV em um DataFrame
+   
     df = pd.read_csv(r"data_pipeline\data\raw\sales_data.csv")  # Definindo df aqui
     print(df.to_string())  # Usando df corretamente
-    return df  # Retornando df para uso posterior 
+    return df  
