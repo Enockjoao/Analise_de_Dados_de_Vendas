e48d69195d4814efb2ed1b52f571316fcd6ac9f9
Enock <108288904+Enockjoao@users.noreply.github.com>
2025-04-08 16:02:16 -0300

    Update load.py

--- scripts/load.py
+++ scripts/load.py
@@ -4,7 +4,7 @@ import sqlite3
 
 
 def load_csv(file_path):
-    # Carregar o arquivo CSV em um DataFrame
+    
     try:
         df = pd.read_csv(file_path, index_col=0)  # Usando a primeira coluna como índice
         print("CSV carregado com sucesso!")
@@ -14,24 +14,24 @@ def load_csv(file_path):
         return None
 
 
-# Caminho do arquivo CSV
+
 csv_path = r"c:\Users\vitor\Desktop\Pasta_de_Projetos\Analise_de_Dados_de_Vendas\data_pipeline\data\raw\sales_data.csv"
 
-# Carregar dados do CSV
+
 df = load_csv(csv_path)
 
 if df is not None:
-    # Criar diretório do banco de dados, se não existir
+    
     os.makedirs('database', exist_ok=True)
 
-    # Caminho do banco de dados
+    
     database_path = os.path.join('database', 'sales.db')
 
-    # Conectar ao banco de dados SQLite
+    
     conn = sqlite3.connect(database_path)
     cursor = conn.cursor()
 
-    # Criar tabela "sales" se não existir
+    
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS sales (
             id INTEGER PRIMARY KEY,
@@ -44,14 +44,14 @@ if df is not None:
     ''')
     print("Tabela criada com sucesso!")
 
-    # Inserir dados do DataFrame na tabela
+    
     try:
         df.to_sql('sales', conn, if_exists='replace', index=False)
         print("Dados inseridos com sucesso no banco de dados!")
     except Exception as e:
         print(f"Erro ao inserir dados: {e}")
 
-    # Fechar conexão
+   
     conn.close()
 else:
     print("Operação cancelada devido a erro no carregamento do CSV.")
