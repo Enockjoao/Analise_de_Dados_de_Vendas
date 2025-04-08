25e20a2fb47a80f22d88c87ef8ba686a2a08f30d
Enock <108288904+Enockjoao@users.noreply.github.com>
2025-04-08 16:03:17 -0300

    Update plot_sales.py

--- visualizations/plot_sales.py
+++ visualizations/plot_sales.py
@@ -3,18 +3,18 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 def plot_sales_by_category(df):
-    # Agrupar os dados por categoria e somar as quantidades
+    
     sales_by_category = df.groupby('category')['quantity'].sum()
 
-    # Criar um gráfico de barras
+   
     sales_by_category.plot(kind='bar', color='skyblue')
 
-    # Adicionar título e rótulos
+    
     plt.title('Quantidade de Produtos Vendidos por Categoria')
     plt.xlabel('Categoria')
     plt.ylabel('Quantidade Vendida')
 
-    # Exibir o gráfico
+    
     plt.show()
 
 def plot_sales_with_seaborn(df):
