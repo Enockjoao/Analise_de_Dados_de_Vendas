a180deb4d1145b336c1c97d32a76762a789187e5
Enock <108288904+Enockjoao@users.noreply.github.com>
2025-04-08 16:02:38 -0300

    Update transform.py

--- scripts/transform.py
+++ scripts/transform.py
@@ -4,10 +4,10 @@ import requests
 
 def transform_data(data):
     data = data.dropna()
-    data["price"] = data["price"].astype(float) # Estou transformando a coluna price em float 
-    data["quantity"] = data["quantity"].astype(int) # Estou transformando a coluna quantity em int
-    data = data.fillna(0) # transformando os Null em 0
-    data["date"] = pd.to_datetime(data["date"]) # transformando a data em modo de data
+    data["price"] = data["price"].astype(float)
+    data["quantity"] = data["quantity"].astype(int) 
+    data = data.fillna(0) 
+    data["date"] = pd.to_datetime(data["date"]) 
     return data
 
 
